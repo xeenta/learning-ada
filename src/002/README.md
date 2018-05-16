2aae6a8140ed0e0fb054d48f28c5accdf98d7cea
xeenta <xeenta@users.noreply.github.com>
2018-05-16 23:20:07 +0200

    mods

--- src/002/README.md
+++ src/002/README.md
@@ -3,4 +3,8 @@ See (add link when generated).
 - `gnatmake -gnat12 -gnatwa greeter.adb`
 - `gnatmake -gnat12 -gnatwa test_pkg.adb`
 - `gnatmake -gnat12 -gnatwa test_person2.adb`
+- `gnatmake -gnat12 -gnatwa n1.ads` (unused: produces only `.o` and `.ali`)
 - `gnatmake -gnat12 -gnatwa thick_hello.adb`
+- `gnatmake -gnat12 -gnatwa test_children.adb`
+- `gnatmake -gnat12 -gnatwa moving_thing.adb` (not part of this article, nonetheless, here it is)
+- `gnatmake -gnat12 -gnatwa test_init.adb`
--- src/002/thick_hello.adb
+++ src/002/thick_hello.adb
@@ -15,17 +15,17 @@ procedure Thick_Hello is
    package body Counter is
       procedure Increment is
       begin
-	 Cnt := Cnt + 1;
+         Cnt := Cnt + 1;
       end Increment;
       
       procedure Increment (I : Integer) is
       begin
-	 Cnt := Cnt + C_Type (I mod C_Type'Modulus);
+         Cnt := Cnt + C_Type (I mod C_Type'Modulus);
       end Increment;
 
       procedure Decrement is
       begin
-	 Cnt := Cnt - 1;
+         Cnt := Cnt - 1;
       end Decrement;
       
       function Value return C_Type is (Cnt);
@@ -38,16 +38,16 @@ begin
    for I in Integer range 1 .. 120
    loop
       if I rem 7 = 0 then
-	 Counter.Decrement;
+         Counter.Decrement;
       elsif I rem 12 = 0 then
-	 Counter.Decrement;
+         Counter.Decrement;
       elsif I rem 17 = 0 then
-	 Counter.Increment;
+         Counter.Increment;
       elsif I rem 25 = 0 then
-	 Counter.Increment (I);
+         Counter.Increment (I);
       end if;
       if T = Counter.Value then
-	 Counter.Increment;
+         Counter.Increment;
       end if;
       M.Put (Counter.Value); New_Line;
       T := Counter.Value;
